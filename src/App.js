33894ca99c0946804ac0915096afabd7fc505c07
ICEBERG776 <henriqueau111@gmail.com>
2023-01-15 17:45:14 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,7 @@ import './App.css';
 
 export default function teste() {
 
- const iSee = () => {
-
-    const texto = "Arroz";
-
-    const button = document.querySelector("button");
-
-    button.addEventListener("click", (evento) => {
-      evento.preventDefault();
-      document.body.innerHTML += `${texto}`;
-      alert("hello world");
-    })
-
- }
-
-iSee();
+ 
 
  const color = () => {
      document.body.style.background = "#2A73D7";
@@ -43,10 +29,27 @@ iSee();
   const showP = () => {
     const inputP = document.querySelector("p");
 
-    inputP.innerHTML = "hello"
+    inputP.innerHTML = "fui enviado";
+    return true;
   }
 
-  showP()
+  const iSee = () => {
+
+    const texto = "Arroz";
+
+    const button = document.querySelector("button");
+
+    button.addEventListener("click", (evento) => {
+      evento.preventDefault();
+      document.body.innerHTML += `${texto}`;
+      alert("hello world");
+
+       showP();
+    })
+
+ }
+
+iSee();
 
   return (
      <div>
